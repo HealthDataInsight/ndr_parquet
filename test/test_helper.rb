4eee9329ef33cdd7ee48f0e1c9fd5a531c4b9345
Tim Gentry <timgentry@users.noreply.github.com>
2021-03-29 22:29:09 +0100

    Added test resources with filesystem paths

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,5 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'ndr_parquet_generator'
 
 require 'minitest/autorun'
+
+SafePath.configure! File.dirname(__FILE__) + '/resources/filesystem_paths.yml'
