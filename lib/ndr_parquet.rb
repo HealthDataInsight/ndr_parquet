ac7bf809cc6636e1a541c3ac5cdbe6dcc5a542aa
Tim Gentry <timgentry@users.noreply.github.com>
2021-04-28 11:47:54 +0100

    Optionally include NdrParquet::S3Wrapper if AWS::S3::Client is available

--- lib/ndr_parquet.rb
+++ lib/ndr_parquet.rb
@@ -3,6 +3,15 @@
 require 'ndr_parquet/generator'
 require 'ndr_parquet/version'
 
+begin
+  # Include NdrParquet::S3Wrapper if Aws::S3::Client available
+  require 'aws-sdk-s3'
+
+  require 'ndr_parquet/s3_wrapper'
+rescue LoadError
+  # do nothing if gem unavailable
+end
+
 # This exposes the root folder for filesystem paths
 module NdrParquet
   def self.root
